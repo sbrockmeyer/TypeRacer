48bbb0e630acd92f2d1c65bf0572fd6eb89ec854
jsingvah <79827219+jsingvah@users.noreply.github.com>
2022-02-16 21:02:10 -0700

    Sign Up form

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 const pug = require('pug');
+const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./routes/routes');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
+const { urlencoded } = require('express');
 
 const app = express();
 
@@ -11,6 +13,10 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(cookieParser('whatever'));
+
+let urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
 
 app.use(expressSession({
     secret: 'c001b34n5',
@@ -23,5 +29,7 @@ app.use(expressSession({
 
 app.get('/', routes.index);
 app.get('/leaderboard', routes.leaderboard);
+app.get('/signup', routes.signup);
+app.post('/signup', urlencodedParser, routes.createUser);
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -2,25 +2,51 @@ const mongoose = require('mongoose');
 const config = require('../config');
 const bcrypt = require('bcryptjs');
 
-mongoose.connect('mongodb+srv://username:pass_word@pro150.rgfcp.mongodb.net/TypeRacerDB?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://username:pass_word@pro150.rgfcp.mongodb.net/TypeRacerDB?retryWrites=true&w=majority', {
     // useUnifiedTopology: true,
     // newUserUrlParser: true
 });
 
-// let mdb = mongoose.connection;
-// mdb.on('error', console.error.bind(console, 'connection error'));
-// mdb.once('opne', callback =>{});
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error'));
+mdb.once('opne', callback => { });
+
+let userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+let User = mongoose.model('Users_Collection', userSchema)
 
 exports.index = (req, res) => {
     res.render('index', {
-        title: 'Home',
+        title: 'Home Page',
         config
     });
 };
 
 exports.leaderboard = (req, res) => {
     res.render('leaderboard', {
-        title: 'Leaderboard',
+        title: 'Leaderboard Page',
         config
     });
+};
+
+exports.signup = (req, res) => {
+    res.render('signup', {
+        title: 'Sign Up Page',
+        config
+    });
+};
+
+exports.createUser = (req, res) => {
+    let user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    user.save((err, user) => {
+        if (err) return console.error(err);
+        console.log(req.body.username + ' added.');
+    });
+    res.redirect('/');
 };
