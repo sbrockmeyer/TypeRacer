379615f92a380cdab470f406bd342cfc4ee01a97
jcao01 <jcao@student.neumont.edu>
2022-02-23 19:19:19 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,34 +8,38 @@
 //     }
 // });
 
-const testQuote = 'Mary had a little lamb';
+const testQuote = 'Mary had a little lamb 123';
 const quoteDisplayElement = document.getElementById('quote');
 const quoteInputElement = document.getElementById('quoteInput');
+const recentCorrectPhraseArray = [];
 
 quoteInputElement.addEventListener('input', () => {
     const quoteArray = quoteDisplayElement.querySelectorAll('span');
     const valueArray = quoteInputElement.value.split('');
     let correct = true;
-    let recentCorrectPhrase = "";
 
     quoteArray.forEach((charSpan, index) => {
         const char = valueArray[index];
         if(char == null) {
             charSpan.classList.remove('correct');
-            quoteInputElement.innerHTML = recentCorrectPhrase;
+            quoteInputElement.value = recentCorrectPhraseArray.join('');
             correct = false;
 
-        } else if (char === charSpan) {
+        } else if (char === charSpan.innerHTML) {
             charSpan.classList.add('correct');
-            recentCorrectPhrase = valueArray.join('');
+            recentCorrectPhraseArray.push(char);
+            correct = true;
         } else {
             charSpan.classList.remove('correct');
             correct = false;
+            quoteInputElement.value = recentCorrectPhraseArray.join('');
         }
     })
+    recentCorrectPhraseArray.length = 0;
 })
 
 function renderQuote() {
+    recentCorrectPhrase = '';
     const quote = testQuote;
     quoteDisplayElement.innerHTML = '';
     quote.split('').forEach(character => {
